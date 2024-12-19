288be259d7857fd01167b9b2d3e4329aee2d334a
MateuszM-ucz <01197834@pw.edu.pl>
2024-12-19 22:09:52 +0100

    Uproszczenie Gaussa

--- src/gauss.c
+++ src/gauss.c
@@ -7,47 +7,24 @@
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-    int i, j, k;
+    double eps = 0.000000001;
     int n = mat->r;
 
-    for(i= 0; i<n; i++){ 			//szukanie najwiekszego elementu
-        double m= fabs(mat->data[i][i]);
-        int w= i;
-        for(k= i+1; k<n; k++){
-            if(fabs(mat->data[k][i]) > m){
-		m= fabs(mat->data[k][i]);
-                w= k;
-            }
-        }
-
-        if(mat->data[w][i] == 0){		//sprawdzanie czy na przekatnej sa zera
+    for(int i=0; i<n; i++){
+        //sprawdzanie czy na przekatnej sa zera
+        if(fabs(mat->data[i][i]) < eps){
             fprintf(stderr, "Macierz jest osobliwa (dzielenie przez zero w wierszu %d)\n", i);
             return 1;
         }
 
-        if(w!=i){				//przenoszenie najwiekszzego elementu na przekatne	
-            double *t= mat->data[i];
-            mat->data[i]= mat->data[w];
-            mat->data[w]= t;
-            double temp= b->data[i][0];
-            b->data[i][0]= b->data[w][0];
-            b->data[w][0]= temp;
-        }
-
-        double y= mat->data[i][i];		//przez co mnozymy
-        for(j=i; j<n; j++){
-            mat->data[i][j] /= y;
-        }
-        b->data[i][0] /= y;
-        for(k= i+1; k<n; k++){			//eliminacja kolumn ponizej przekatnej
-            double factor= mat->data[k][i];
-            for(j=i; j<n; j++){
-                mat->data[k][j]-=factor*mat->data[i][j];
+        //schodkowanie
+        for(int j=i+1; j<n; j++){
+            double factor = mat->data[j][i]/mat->data[i][i];
+            for(int k=i; k<n; k++){
+                mat->data[j][k] -= factor*mat->data[i][k];
             }
-            b->data[k][0]-=factor*b->data[i][0];
+            b->data[j][0] -= factor*b->data[i][0];
         }
     }
     return 0;
 }
-
-
